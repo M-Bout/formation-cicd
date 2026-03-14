5443431d44481fdb1645b69c594583f8351dddbe
M-Bout <abibah.med@hotmail.com>
2026-03-14 00:26:31 -0400

    Fix: Erreur du code

--- formation-cicd/Program.cs
+++ formation-cicd/Program.cs
@@ -7,7 +7,7 @@ var calculatrice = new Calculatrice(); // On utilise ta classe de Calculatrice.c
 
 app.MapGet("/", () => "Serveur Web de M-Bout prêt !");
 
-aapp.MapGet("/somme", (int a, int b) =>
+app.MapGet("/somme", (int a, int b) =>
 {
     int resultat = calculatrice.Additionner(a, b);
     return Results.Ok(new { operation = $"{a} + {b}", resultat = resultat });
