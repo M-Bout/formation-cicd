c67e8b57a65d2438b48ff846ee2f1df66ae647a7
M-Bout <abibah.med@hotmail.com>
2026-03-14 00:17:56 -0400

    Fix: formatage du code

--- formation-cicd/Program.cs
+++ formation-cicd/Program.cs
@@ -8,6 +8,7 @@ var calculatrice = new Calculatrice(); // On utilise ta classe de Calculatrice.c
 app.MapGet("/", () => "Serveur Web de M-Bout prêt !");
 
 app.MapGet("/somme", (int a, int b) => 
+
 {
     int resultat = calculatrice.Additionner(a, b);
     return Results.Ok(new { operation = $"{a} + {b}", resultat = resultat });
