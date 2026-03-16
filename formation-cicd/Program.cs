5f7615c61e591029e75846cfdd06e03ad9c0742c
M-Bout <abibah.med@hotmail.com>
2026-03-16 00:10:31 -0400

    Ajout de la cle secret-producrion

--- formation-cicd/Program.cs
+++ formation-cicd/Program.cs
@@ -1,6 +1,10 @@
 ﻿using MyProject.Logic; // On importe ton namespace ici !
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Récupérer une valeur nommée "GedSettings:EncryptionKey"
+string encryptionKey = builder.Configuration["GedSettings:EncryptionKey"] ?? "Clé_Par_Défaut_Locale";
+
 var app = builder.Build();
 
 var calculatrice = new Calculatrice(); // On utilise ta classe de Calculatrice.cs
@@ -12,5 +16,7 @@ app.MapGet("/somme", (int a, int b) =>
     int resultat = calculatrice.Additionner(a, b);
     return Results.Ok(new { operation = $"{a} + {b}", resultat = resultat });
 });
+
+app.MapGet("/config-test", () => $"La clé utilisée est : {encryptionKey}");
 
 app.Run();
